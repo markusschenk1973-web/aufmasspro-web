dc345ceabeadc591140f3682a3357f5a06275093
markusschenk1973-web <277443863+markusschenk1973-web@users.noreply.github.com>
2026-09-09 14:06:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // AufmaßPro Service Worker
-const CACHE = 'aufmasspro-v2';
+const CACHE = 'aufmasspro-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
